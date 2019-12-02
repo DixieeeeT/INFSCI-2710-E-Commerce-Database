2d05b38870fcf5b33c745e7b38e718d0af3937f6
Danlan <dah189@pitt.edu>
2019-12-02 11:40:12 -0500

    Update addSuccess.php
    adjust navbar

--- addSuccess.php
+++ addSuccess.php
@@ -8,12 +8,31 @@
 		<title>Edit Success</title>
 	</head>
 	<body>
-		<header class = "container" style = "text-align: center">
-			<h3 class = "col" style="display:inline">XXX's Cars</h3>&nbsp;&nbsp;
-			<a class = "col" href="customers.php">Customer Interface</a>&nbsp;&nbsp;
-			<a class = "col" href="data.php">Data Aggregation</a>&nbsp;&nbsp;
-			<a class = "col" href="employees.php">Employee Login</a>&nbsp;&nbsp;
-		</header>
+		<header class="container">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="customers.php">XXX's Cars</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="customers.php">Main Page</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="data.php">Data Aggregation</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="employees.php">Additional Actions</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="startpage.php">Back to Start Page</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</header>
+		
 		<h1 class = "text-center" style = "margin-top: 200px">
 			<b>
 				Addition Proceeds Successfully. <br><br>
